eee0b07d2e5a38a285c25229319e00ce5aaebd99
Neocode009 <9lightworks@gmail.com>
2023-08-01 19:18:26 +0530

    Commit

--- STRINGS/STRING-PROBLEMS/src/string/problem_1/StringProblem_1.java
+++ STRINGS/STRING-PROBLEMS/src/string/problem_1/StringProblem_1.java
@@ -9,7 +9,8 @@ public class StringProblem_1 {
         String str = "This is an example string. Another example of the word example.";
         int count = 0;
         int index = -1;
-        while ((index = str.indexOf("example", index + 1)) != -1 && count < 4) {
+        while ((index = str.indexOf("example", index + 1)) != -1 && count < 4)
+        {
             System.out.println("Occurrence " + (count + 1) + " found at index: " + index);
             count++;
         }
---
+++ STRINGS/STRING-PROBLEMS/src/string/problem_3/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created By Abhijeet Raut on || Date : 01-08-2023 ||  Time : 07:18 pm.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
